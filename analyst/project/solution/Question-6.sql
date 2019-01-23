2183bfa615c1f4c38d86e06e06c883aedaae34f6
Andy Ia <andyia475@gmail.com>
2019-01-22 23:37:02 -0500

    Revert "Question 6 Solution"
    This reverts commit 655463f03b2dc90882ca03ed2821d71bd81cc8a4.

--- analyst/project/solution/Question-6.sql
+++
@@ -1,9 +0,0 @@
-/***6. How many confirmed completed reservations did the member (ID) with the most reserved classes in February have?***/
-
-select 
-	memberidentifier,
-    SUM(checked_in) AS confirmed_classes
-from peerfit.all_reservations AS all_reservations
-where CAST(all_reservations.class_time_at AS DATE) >= '2018-02-01' AND CAST(all_reservations.class_time_at AS DATE) < '2018-03-01'
-group by all_reservations.memberidentifier
-order by SUM(checked_in) DESC
--- analyst/project/solution/all_reservations.sql
+++ analyst/project/solution/all_reservations.sql
@@ -6,7 +6,6 @@ CREATE TABLE all_reservations AS
 select  *
 from	(select
             concat("mindbody",id) as uniqueidentifier,
-            concat("mindbody",member_id) as memberidentifier,
             "mindbody" AS location,
 			id,
 			member_id,
@@ -23,6 +22,7 @@ from	(select
 			canceled_at,
 			CASE WHEN canceled_at IS NULL THEN 0
 				ELSE 1 END AS cancelled,
+			NULL AS reserved_for,
 			class_time_at,
 			checked_in_at,
             CASE WHEN checked_in_at IS NULL THEN 0
@@ -32,8 +32,7 @@ from	(select
 		UNION ALL
 
 		select
-			concat("clubready",id) AS uniqueidentifier,
-            concat("clubready",member_id) AS memberidentifier,
+			concat("clubready",id) as uniqueidentifier,
             "clubready" AS location,
 			id,
 			member_id,
@@ -50,7 +49,8 @@ from	(select
             NULL AS canceled_at,
 			CASE WHEN canceled = 't' THEN 1
 				ELSE 0 END AS canceled,
-			reserved_for AS class_time_at,
+			reserved_for,
+			NULL AS class_time_at,
 			signed_in_at,
             CASE WHEN signed_in_at IS NULL THEN 0
 				ELSE 1 END AS checked_in

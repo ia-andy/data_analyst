d376cc92b3781ef09cc65dc14792fb172fe73bdb
Andy Ia <andyia475@gmail.com>
2019-01-23 08:41:03 -0500

    Update Question-6.sql
    Formatting update

--- analyst/project/solution/Question-6.sql
+++ analyst/project/solution/Question-6.sql
@@ -1,9 +1,9 @@
 /***6. How many confirmed completed reservations did the member (ID) with the most reserved classes in February have?***/
 
-select 
+SELECT
 	memberidentifier,
     SUM(checked_in) AS confirmed_classes
-from peerfit.all_reservations AS all_reservations
-where CAST(all_reservations.class_time_at AS DATE) >= '2018-02-01' AND CAST(all_reservations.class_time_at AS DATE) < '2018-03-01'
-group by all_reservations.memberidentifier
-order by SUM(checked_in) DESC
+FROM	peerfit.all_reservations AS all_reservations
+WHERE CAST	(all_reservations.class_time_at AS DATE) >= '2018-02-01' AND CAST(all_reservations.class_time_at AS DATE) < '2018-03-01'
+GROUP BY	all_reservations.memberidentifier
+ORDER BY	SUM(checked_in) DESC

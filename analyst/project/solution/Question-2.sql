bfe37e60438e5acb825e74c2f19703275149984d
Andy Ia <andyia475@gmail.com>
2019-01-23 08:33:53 -0500

    Update Question 2
    Formatting update

--- analyst/project/solution/Question-2.sql
+++ analyst/project/solution/Question-2.sql
@@ -1,11 +1,11 @@
 /***2. Which studio has the highest rate of reservation abandonment (did not cancel but did not check-in)?***/
 
-select
-	studio_key,
+SELECT
+	 studio_key,
     SUM(CASE WHEN cancelled = 0 THEN 1 ELSE 0 END) AS NotCancelled,
     SUM(CASE WHEN checked_in = 0 AND cancelled = 0 THEN 1 ELSE 0 END) AS NotChecked_in,
     (SUM(CASE WHEN checked_in = 0 AND cancelled = 0 THEN 1 ELSE 0 END))/(SUM(CASE WHEN cancelled = 0 THEN 1 ELSE 0 END)) AS AbandonRate
-from peerfit.all_reservations AS all_reservations
-where all_reservations.studio_key IS NOT NULL
-group by all_reservations.studio_key
-order by AbandonRate Desc
+FROM peerfit.all_reservations AS all_reservations
+WHERE all_reservations.studio_key IS NOT NULL
+GROUP BY all_reservations.studio_key
+ORDER BY AbandonRate DESC

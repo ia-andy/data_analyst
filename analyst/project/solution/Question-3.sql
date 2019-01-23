5b0834c15e2bb52bc4bba021a2931e6eee70cd3a
Andy Ia <andyia475@gmail.com>
2019-01-23 08:36:04 -0500

    Update Question-3.sql
    Formatting update

--- analyst/project/solution/Question-3.sql
+++ analyst/project/solution/Question-3.sql
@@ -1,10 +1,10 @@
 /***3. Which fitness area (i.e., tag) has the highest number of completed reservations for February?***/
 
-select
+SELECT
 	class_tag,
     COUNT(uniqueidentifier) AS COUNT
-from peerfit.all_reservations AS all_reservations
-where (CAST(all_reservations.checked_in_at AS DATE) > '2018-02-01'
+FROM	peerfit.all_reservations AS all_reservations
+WHERE	(CAST(all_reservations.checked_in_at AS DATE) > '2018-02-01'
 		AND CAST(all_reservations.checked_in_at AS DATE) < '2018-03-01')
-group by all_reservations.class_tag
-order by COUNT(uniqueidentifier) DESC
+GROUP BY	all_reservations.class_tag
+ORDER BY COUNT(uniqueidentifier) DESC

226d459f0d5aed396580246600d2e7ce8204cd42
Andy Ia <andyia475@gmail.com>
2019-01-23 08:34:36 -0500

    Update Question-1.sql
    Formatting update

--- analyst/project/solution/Question-1.sql
+++ analyst/project/solution/Question-1.sql
@@ -1,7 +1,7 @@
 /***1. Across all reservation partners for January & February, how many completed reservations occurred?***/
 
 select  SUM(all_reservations.checked_in) AS Completed_Reservations
-from	peerfit.all_reservations as all_reservations
+from	peerfit.all_reservations AS all_reservations
 where 	CAST(all_reservations.checked_in_at AS DATE) >= '2018-01-01'
 		AND CAST(all_reservations.checked_in_at AS DATE) < '2018-03-01'
 
